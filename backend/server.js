cfae3c91a601c8200a49385bf70df9fb9a4d6d6b
Shubham Kumar <kumar19subham@gmail.com>
2026-02-25 01:19:45 +0530

    Added CI/CD pipeline

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,36 @@
 const express = require("express");
-//const cors = require("cors");
 
 const app = express();
 
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
+
+// Use environment variable for Mongo URI
+const MONGO_URI = process.env.MONGO_URI || db.url;
+
 db.mongoose
-  .connect(db.url, {
+  .connect(MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to the database!");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
 
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Test application." });
 });
 
 require("./app/routes/turorial.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
